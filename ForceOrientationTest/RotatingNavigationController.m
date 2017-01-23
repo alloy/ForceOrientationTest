4c5cc61c6d4f99156477d5d2bf4ca14d90c495a7
tom.zhu <tomzhu@vipabc.com>
2017-01-23 17:18:13 +0800

    [bug fix]avoid crash

--- ForceOrientationTest/RotatingNavigationController.m
+++ ForceOrientationTest/RotatingNavigationController.m
@@ -3,6 +3,8 @@
 // Use a dummy class, because this workaround will also trigger calls to
 // `viewWillAppear:` & `viewDidAppear:`, which might trigger side-effects in
 // the actual controller that the user wants displayed.
+static BOOL SHOULD_POP = YES;
+
 @interface DummyController : UIViewController
 @property (assign) NSUInteger supportedInterfaceOrientations;
 @end
@@ -19,51 +21,60 @@
 
 - (void)forceSupportedOrientation:(UIViewController *)controller;
 {
-  if (self.dummyController == nil) {
-    self.dummyController = [DummyController new];
-  }
-  self.dummyController.supportedInterfaceOrientations = controller.supportedInterfaceOrientations;
-  [self presentViewController:self.dummyController animated:NO completion:NULL];
-  [self.dummyController dismissViewControllerAnimated:NO completion:NULL];
+    if (self.dummyController == nil) {
+        self.dummyController = [DummyController new];
+    }
+    self.dummyController.supportedInterfaceOrientations = controller.supportedInterfaceOrientations;
+    //[self presentViewController:self.dummyController animated:NO completion:NULL];
+    //[self.dummyController dismissViewControllerAnimated:NO completion:NULL];
+    SHOULD_POP = NO;
+    [self presentViewController:self.dummyController animated:NO completion:^{
+        [self.dummyController dismissViewControllerAnimated:NO completion:^{
+            SHOULD_POP = YES;
+        }];
+    }];
 }
 
 - (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated;
 {
-  if (self.viewControllers.count > 0) {
-    [self forceSupportedOrientation:viewController];
-  }
-  [super pushViewController:viewController animated:animated];
+    [super pushViewController:viewController animated:animated];
+    if (self.viewControllers.count > 0) {
+        [self forceSupportedOrientation:viewController];
+    }
 }
 
 - (UIViewController *)popViewControllerAnimated:(BOOL)animated;
 {
-  [self forceSupportedOrientation:self.viewControllers[self.viewControllers.count - 2]];
-  return [super popViewControllerAnimated:animated];
+    if (!SHOULD_POP) {
+        return [super popViewControllerAnimated:animated];
+    }
+    [self forceSupportedOrientation:self.viewControllers[self.viewControllers.count - 2]];
+    return [super popViewControllerAnimated:animated];
 }
 
 - (BOOL)shouldAutorotate;
 {
-  return self.visibleViewController.shouldAutorotate;
+    return self.visibleViewController.shouldAutorotate;
 }
 
-- (NSUInteger)supportedInterfaceOrientations;
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations;
 {
-  return self.visibleViewController.supportedInterfaceOrientations;
+    return self.visibleViewController.supportedInterfaceOrientations;
 }
 
 - (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration;
 {
-  [self.visibleViewController willRotateToInterfaceOrientation:toInterfaceOrientation duration:duration];
+    [self.visibleViewController willRotateToInterfaceOrientation:toInterfaceOrientation duration:duration];
 }
 
 - (void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation duration:(NSTimeInterval)duration;
 {
-  [self.visibleViewController willAnimateRotationToInterfaceOrientation:interfaceOrientation duration:duration];
+    [self.visibleViewController willAnimateRotationToInterfaceOrientation:interfaceOrientation duration:duration];
 }
 
 - (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation;
 {
-  [self.visibleViewController didRotateFromInterfaceOrientation:fromInterfaceOrientation];
+    [self.visibleViewController didRotateFromInterfaceOrientation:fromInterfaceOrientation];
 }
 
 @end
